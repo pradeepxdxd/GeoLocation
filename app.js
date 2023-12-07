7d8a184b3cafd91ddbf47cb88e49c01b474bc65b
pradeep <biswas.pradeep@neosoftmail.com>
2023-12-07 12:39:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { SuperfaceClient } from "@superfaceai/one-sdk";
+const app = express();
+app.set("trust proxy", true);
+
+const sdk = new SuperfaceClient();
+
+async function run(ip) {
+    // Load the profile
+    const profile = await sdk.getProfile("address/ip-geolocation@1.0.1");
+    console.log(profile)
+    // Use the profile
+    const result = await profile.getUseCase("IpGeolocation").perform(
+        {
+            ipAddress: ip
+        },
+        {
+            provider: "ipdata",
+            security: {
+                apikey: {
+                    apikey: "26d3e5db3eaca3ad463c360a5081a313e91fed287dcabd71c8b76bf3"
+                }
+            }
+        }
+    );
+
+    // Handle the result
+    try {
+        const data = result.unwrap();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.get("/", async (req, res) => {
+    res.send(await run(req.ip));
+});
+
+app.listen(3000, () => {
+    console.log("SERVER RUNNIHG AT PORT 3000");
+});
